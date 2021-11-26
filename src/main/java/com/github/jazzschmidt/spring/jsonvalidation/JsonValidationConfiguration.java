9118aef024c8c073f1f03988279039c842e98767
Carsten Schmidt <jazzschmidt@icloud.com>
2021-11-26 15:52:08 +0100

    First unimportant changes

---
+++ src/main/java/com/github/jazzschmidt/spring/jsonvalidation/JsonValidationConfiguration.java
@@ -0,0 +1,8 @@
+package com.github.jazzschmidt.spring.jsonvalidation;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JsonValidationConfiguration {
+    public boolean enabled = true;
+}
